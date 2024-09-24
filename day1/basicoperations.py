ce3e25e21013f880545272d0ef3f07a3eee8f407
muchaisam <samson.muchai@strathmore.edu>
2024-09-24 09:10:17 +0300

    Add basicoperations.py file with Python data types and their examples

---
+++ day1/basicoperations.py
@@ -0,0 +1,20 @@
+#python data types
+
+x = 5  #integers-whole numbers
+y = 3.14 #float-decimal numbers
+name = "Sammy" #string
+is_active = True #boolean (true or false vals)
+my_list = [1, 2, 3, 4, 5] #collection of items
+my_tuple= (1,2,3) #immutable collection of items
+my_dict = {'name': 'Sammy', 'age': 50} #key-value pairs
+my_set = {1, 2, 3, 4} #unordered collection of unique items
+
+#print values and their types
+print(f"x is {x} and its type is {type(x)}")
+print(f"y is {y} and its type is {type(y)}")
+print(f"name is {name} and its type is {type(name)}")
+print(f"is_active is {is_active} and its type is {type(is_active)}")
+print(f"my_list is {my_list} and its type is {type(my_list)}")
+print(f"my_tuple is {my_tuple} and its type is {type(my_tuple)}")
+print(f"my_dict is {my_dict} and its type is {type(my_dict)}")
+print(f"my_set is {my_set} and its type is {type(my_set)}")
