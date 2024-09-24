4f63d6b1856db10d0167d75bdec8af3f873deff4
muchaisam <samson.muchai@strathmore.edu>
2024-09-24 09:11:21 +0300

    Add basic arithmetic, comparison, and logical operations in varops.py

---
+++ day1/varops.py
@@ -0,0 +1,32 @@
+#arithmetic : +, -, *, /, %(modulo), **(exponentiation), //
+#comparison : ==, !=, >, <, >=, <=
+#logical : and, or, not
+
+#Arithmetic Operators
+x = 10
+y = 3
+
+print(f"Addition : {x + y}")
+print(f"Subtraction : {x - y}")
+print(f"multiplication : {x * y}")
+print(f"Division : {x / y}")
+print(f"Modulo : {x % y}")
+print(f"Exponentiation : {x ** y}")
+
+
+#Comparison Operators
+print(f"Equal : {x == y}")
+print(f"Not Equal : {x != y}")
+print(f"Greater than : {x > y}")
+print(f"Less than : {x < y}")
+print(f"Greater than or Equal : {x >= y}")
+print(f"Less than or Equal : {x <= y}")
+
+
+#Logical Operators
+a = True
+b = False
+
+print(f"AND : {a and b}")
+print(f"OR : {a or b}")
+print(f"NOT : {not a}")
